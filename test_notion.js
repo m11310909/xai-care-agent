7b7c6f0e64750f2d36c5f02a3c68a9b9649933ff
m11310909 <m11310909@gapps.ntust.edu.tw>
2025-06-17 15:57:50 +0800

    Update test_notion.js
    修正 Notion logger 呼叫錯誤"

--- test_notion.js
+++ test_notion.js
@@ -2,7 +2,3 @@ require('dotenv').config();
 const { logCommitsToNotion } = require('notion-logger');
 
 logCommitsToNotion();
-
-const notionLogger = require('notion-logger');
-
-notionLogger.log(); // 就會自動將最近 commit 寫入 Notion
